8a64e9a41454ee5a0ded94aa4f32422a0979a8e5
Rory Shanks <megalan247@users.noreply.github.com>
2018-09-12 09:57:48 +0200

    Added x-client-ip to updator function

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -15,7 +15,7 @@ exports.createPlayer = function(req, res) {
 }
 
 exports.updatePlayer = function(req, res) {
-  var ip = (req.headers['X-Forwarded-For'] || req.connection.remoteAddress || '').split(',')[0].trim();
+  var ip = (req.headers['x-client-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
   var query = "UPDATE `db_displaymanager`.`tb_players` SET `player_lastPing`=" + SqlString.escape(Date.now()) + ", `player_type`=" + SqlString.escape(req.body.playerType) + ", `player_serialnumber`=" + SqlString.escape(req.body.serialNumber) + ", `player_OS`=" + SqlString.escape(req.body.OSName) + ", `player_OSVersion`=" + SqlString.escape(req.body.OSVersion) + ", `player_freeSpace`=" + SqlString.escape(req.body.freeSpace) + ", `player_CPU`=" + SqlString.escape(req.body.cpu) + ", `player_IP`=" + SqlString.escape(ip.split(':').pop()) +", `player_MAC`=" + SqlString.escape(req.body.macAddress) + " WHERE `player_id`=" + SqlString.escape(req.body.id) + ";";
   con.query(query, function (err, result) {
     res.send(err)
@@ -36,7 +36,7 @@ exports.registerPlayer = function(req, res) {
     // name, playerType, playerLocation, serialNumber, OSName, OSVersion, freeSpace, CPUPercentage
     require('crypto').randomBytes(3, function(err, buffer) {
         var token = buffer.toString('hex');
-        var ip = (req.headers['X-Forwarded-For'] || req.headers['X-Client-IP'] || req.connection.remoteAddress || '').split(',')[0].trim();
+        var ip = (req.headers['x-client-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
         //var sqlQuery = "INSERT INTO `db_displaymanager`.`tb_players` (`player_id`, `player_name`, `player_description`, `player_lastPing`, `player_type`, `player_location`, `player_serialnumber`, `player_OS`, `player_OSVersion`) VALUES ('" + token + "', " + SqlString.escape(req.body.name) + ", 'This is a placeholder description', '" + SqlString.escape(Date.now()) + "', " + SqlString.escape(req.body.playerType) + ", 'Unknown player location' " + SqlString.escape(req.body.serialNumber) + ", " + SqlString.escape(req.body.OSName) + ", " + SqlString.escape(req.body.OSVersion) + ");"
         var sql = "INSERT INTO `db_displaymanager`.`tb_players` (`player_id`, `player_name`, `player_description`, `player_lastPing`, `player_type`, `player_location`, `player_serialnumber`, `player_roomId`, `player_OS`, `player_OSVersion`, `player_freeSpace`, `player_CPU`, `player_IP`, `player_MAC`) VALUES ('" + token + "', " + SqlString.escape(req.body.name) + ", 'Desciption', '" + SqlString.escape(Date.now()) + "', " + SqlString.escape(req.body.playerType) + ", 'Location', " + SqlString.escape(req.body.serialNumber) + ", 'temprm', " + SqlString.escape(req.body.OSName) + ", " + SqlString.escape(req.body.OSVersion) + ", 'freeSpace', 'CPU', " + SqlString.escape(ip.split(':').pop()) +", " + SqlString.escape(req.body.macAddress) + ");"
         console.log(sql);
